7658d0871ccd7984d1e00d2346770edacdca5187
zhouhaonan <zhn95330@126.com>
2020-09-23 11:29:46 +0800

    第san次

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,53 @@
 ;
+//创建组件
 let mainCpn = Vue.extend({
     template:'#mainTemp'
 });
 
-let mainnav = Vue.component('mainnav',mainCpn)
+let goTopCpn = Vue.extend({
+    template:'#goTopTemp',
+    data:function () {
+        return{
+            topDistance:0
+        };
+    },
+    mounted(){
+        window.addEventListener('scroll',this.getTopDistance);
+    },
+    methods:{
+        getTopDistance:function () {
+            this.topDistance = document.documentElement.scrollTop||document.body.scrollTop||window.pageYOffset
+        },
+        gotop:function () {
+            
+            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0;
+            window.pageYOffset = 0;
+        }
+    }
+});
+
+
+//注册
+let mainnavcpn = Vue.component('mainnavcpn',mainCpn)
+let gotopcpn = Vue.component('gotopcpn',goTopCpn)
+
+
+
+
+
 const vm = new Vue({
     el:"#app",
     data:{
     
     }
 });
+
+//jQuery实现回到顶部
+$(window).ready(function () {
+    $(window).scroll(function () {
+        $(window).scrollTop();
+    });
+});
+
+
