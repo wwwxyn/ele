dc86873fdd00fe79d7c25f0876033d709bcb0837
zhouhaonan <zhn95330@126.com>
2020-09-23 16:23:54 +0800

    第san次

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,31 @@
 ;
 //创建组件
+
+let topBannerCpn = Vue.extend({
+    template: '#topBannerTemp',
+    data:function () {
+        return {
+            searchBarFixed:false,
+        };
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+        handleScroll () {
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+            var offsetTop = document.querySelector('#searchBar').offsetTop
+            if (scrollTop >= offsetTop) {
+                this.searchBarFixed = true
+            } else {
+                this.searchBarFixed = false
+            }
+            // console.log(scrollTop,offsetTop)
+        },
+    }
+    
+});
+
 let mainCpn = Vue.extend({
     template:'#mainTemp'
 });
@@ -19,7 +45,6 @@ let goTopCpn = Vue.extend({
             this.topDistance = document.documentElement.scrollTop||document.body.scrollTop||window.pageYOffset
         },
         gotop:function () {
-            
             document.documentElement.scrollTop = 0;
             document.body.scrollTop = 0;
             window.pageYOffset = 0;
@@ -31,7 +56,7 @@ let goTopCpn = Vue.extend({
 //注册
 let mainnavcpn = Vue.component('mainnavcpn',mainCpn)
 let gotopcpn = Vue.component('gotopcpn',goTopCpn)
-
+let topBannercpn = Vue.component('topbannercpn',topBannerCpn)
 
 
 
@@ -44,10 +69,10 @@ const vm = new Vue({
 });
 
 //jQuery实现回到顶部
-$(window).ready(function () {
-    $(window).scroll(function () {
-        $(window).scrollTop();
-    });
-});
+// $(window).ready(function () {
+//     $(window).scroll(function () {
+//         $(window).scrollTop();
+//     });
+// });
 
 
